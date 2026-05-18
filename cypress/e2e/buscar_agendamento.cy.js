ee9dddbba55adf43123c7cfd6356f95ea8c54823
thiagogimenes <thiagoaticus@gmail.com>
2026-05-17 22:41:06 -0300

    add cenários para busca de agendamento

--- cypress/e2e/buscar_agendamento.cy.js
+++ cypress/e2e/buscar_agendamento.cy.js
@@ -1,32 +1,44 @@
-/// < reference types="cypress"/>
+/// <reference types="cypress"/>
+
+const agendamento_sucesso = require("../fixtures/agendamento_sucesso_payload.json")
 
 describe('Buscar agendamento', () => {
-    
-    it('Buscar agendamento com sucesso', () => {
-        
+
+    it('Deve buscar agendamento com sucesso', () => {
+
         cy.request({
-            method: "GET",
-            url: "https://restful-booker.herokuapp.com/booking/12",
-        })
-        .then((resposta) => {
+            method: "POST",
+            url: "https://restful-booker.herokuapp.com/booking",
+            body: agendamento_sucesso,
+        }).then((resposta) => {
             expect(resposta.status).to.equal(200)
-            expect(resposta.body.firstname).to.equal("John")
-            expect(resposta.body.lastname).to.equal("Smith")
-            expect(resposta.body.totalprice).to.equal(111)
-            expect(resposta.body.bookingdates.checkin).to.equal("2018-01-01")
-            expect(resposta.body.bookingdates.checkout).to.equal("2019-01-01")
-            expect(resposta.body.additionalneeds).to.equal("Breakfast")
+
+            const id = resposta.body.bookingid
+
+            cy.request({
+                method: "GET",
+                url: `https://restful-booker.herokuapp.com/booking/${id}`,
+            })
+                .then((resposta) => {
+                    expect(resposta.status).to.equal(200)
+                    expect(resposta.body.firstname).to.equal(agendamento_sucesso.firstname)
+                    expect(resposta.body.lastname).to.equal(agendamento_sucesso.lastname)
+                    expect(resposta.body.totalprice).to.equal(agendamento_sucesso.totalprice)
+                    expect(resposta.body.bookingdates.checkin).to.equal(agendamento_sucesso.bookingdates.checkin)
+                    expect(resposta.body.bookingdates.checkout).to.equal(agendamento_sucesso.bookingdates.checkout)
+                    expect(resposta.body.additionalneeds).to.equal(agendamento_sucesso.additionalneeds)
+                })
         })
     });
 
-    it('Buscar agendamento inexistente', () => {
-         cy.request({
+    it('Não deve buscar agendamento inexistente', () => {
+        cy.request({
             method: "GET",
             url: "https://restful-booker.herokuapp.com/booking/xpto",
             failOnStatusCode: false,
         })
-        .then((resposta) => {
-            expect(resposta.status).to.equal(404)
-        })
+            .then((resposta) => {
+                expect(resposta.status).to.equal(404)
+            })
     });
 });
