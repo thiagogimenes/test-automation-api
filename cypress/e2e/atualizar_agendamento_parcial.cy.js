af6b084c26e2e2f3b721a61600f7086bfbab2c84
thiagogimenes <thiagoaticus@gmail.com>
2026-05-17 22:40:55 -0300

    add cenários de agendamento parcial

---
+++ cypress/e2e/atualizar_agendamento_parcial.cy.js
@@ -0,0 +1,63 @@
+/// <reference types="cypress" /> 
+
+const agendamento_sucesso = require("../fixtures/agendamento_sucesso_payload.json")
+
+describe('Atualizar agendamento', () => {
+
+    let token = ''
+
+    beforeEach(() => {
+
+        cy.request({
+            method: "POST",
+            url: "https://restful-booker.herokuapp.com/auth",
+            body: {
+                username: "admin",
+                password: "password123"
+            }
+        }).then((resultado) => {
+            token = resultado.body.token
+        })
+    })
+
+    it('Deve atualizar nome e sobrenome do agendamento', () => {
+
+        cy.request({
+            method: "POST",
+            url: "https://restful-booker.herokuapp.com/booking",
+            body: agendamento_sucesso,
+        }).then((resposta) => {
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.booking.firstname).to.equal(agendamento_sucesso.firstname)
+            expect(resposta.body.booking.lastname).to.equal(agendamento_sucesso.lastname)
+
+            const id = resposta.body.bookingid
+
+            cy.log("Cadastrando agendamento")
+
+            cy.request({
+                method: "PATCH",
+                url: `https://restful-booker.herokuapp.com/booking/${id}`,
+                headers: {
+                    "cookie": `token=${token}`,
+                },
+                body: {
+                    "firstname": "Jorge",
+                    "lastname": "Bravo"
+                }
+            })
+                .then((resposta) => {
+                    expect(resposta.status).to.equal(200)
+                    expect(resposta.body.firstname).to.equal("Jorge")
+                    expect(resposta.body.lastname).to.equal("Bravo")
+                    expect(resposta.body.totalprice).to.equal(agendamento_sucesso.totalprice)
+                    expect(resposta.body.bookingdates.checkin).to.equal(agendamento_sucesso.bookingdates.checkin)
+                    expect(resposta.body.bookingdates.checkout).to.equal(agendamento_sucesso.bookingdates.checkout)
+                    expect(resposta.body.additionalneeds).to.equal(agendamento_sucesso.additionalneeds)
+                    cy.log("Agendamento alterado")
+                })
+        })
+
+
+    });
+});
