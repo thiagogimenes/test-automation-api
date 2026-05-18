0ac877bd125b96dab0f38fbda29257a643a9a82d
thiagogimenes <thiagoaticus@gmail.com>
2026-05-17 22:41:32 -0300

    add cenário para deletar agendamento

---
+++ cypress/e2e/deletar_agendamento.cy.js
@@ -0,0 +1,97 @@
+/// <reference types="cypress" /> 
+
+const agendamento_sucesso = require("../fixtures/agendamento_sucesso_payload.json")
+
+describe('Deletar agendamento', () => {
+
+        let token = ''
+
+        beforeEach(( ) => {
+
+            cy.request({
+                method: "POST",
+                url: "https://restful-booker.herokuapp.com/auth",
+                body: {
+                    username: "admin",
+                    password: "password123"
+                }
+            }).then((resultado) => {
+                token = resultado.body.token
+            })
+        })
+
+    it('Deve deletar agendamento com sucesso', () => {
+        cy.request({
+            method: "POST",
+            url: "https://restful-booker.herokuapp.com/booking",
+            body: agendamento_sucesso,
+        }).then((resposta) => {
+            expect(resposta.status).to.equal(200)
+
+            const id = resposta.body.bookingid
+
+            cy.request({
+                method: "DELETE",
+                url: `https://restful-booker.herokuapp.com/booking/${id}`,
+                headers: {
+                    "cookie": `token=${token}`,
+                }
+            })
+                .then((resposta) => {
+                    expect(resposta.status).to.equal(201)
+                });
+        })
+    });
+
+    it('Não deve deletar um registros inexistente', () => {
+        cy.request({
+                method: "DELETE",
+                url: `https://restful-booker.herokuapp.com/booking/xpto`,
+                headers: {
+                    "cookie": `token=${token}`,
+                },
+                failOnStatusCode: false,
+            })
+                .then((resposta) => {
+                    expect(resposta.status).to.equal(405)
+                });
+
+    });
+
+    it.only('Não deve deletar um registro já deletado', () => {
+        cy.request({
+            method: "POST",
+            url: "https://restful-booker.herokuapp.com/booking",
+            body: agendamento_sucesso,
+        }).then((resposta) => {
+            expect(resposta.status).to.equal(200)
+
+            const id = resposta.body.bookingid
+
+            cy.request({
+                method: "DELETE",
+                url: `https://restful-booker.herokuapp.com/booking/${id}`,
+                headers: {
+                    "cookie": `token=${token}`,
+                }
+            })
+                .then((resposta) => {
+                    expect(resposta.status).to.equal(201)
+                    cy.log("Registro apagado")
+                });
+            
+               cy.request({
+                method: "DELETE",
+                url: `https://restful-booker.herokuapp.com/booking/${id}`,
+                headers: {
+                    "cookie": `token=${token}`,
+                },
+                failOnStatusCode: false,
+            })
+                .then((resposta) => {
+                    expect(resposta.status).to.equal(405)
+                    cy.log("Tentando apagar o registro novamente")
+                });
+        })
+    });
+});
