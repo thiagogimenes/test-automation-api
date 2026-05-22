5cf05c6b06c5a2660180cc1f51422ca9eb924e92
thiagogimenes <thiagoaticus@gmail.com>
2026-05-21 22:54:26 -0300

    refactory testes de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,41 +3,22 @@
 describe('Realizar login', () => {
 
     it('Deve realizar login com sucesso', () => {
-        cy.request({
-            method: "POST",
-            url: "https://restful-booker.herokuapp.com/auth",
-            body: {
-                username: "admin",
-                password: "password123"
-            }
-        }).then((resultado) => {
+        cy.Login("admin", "password123")
+        .then((resultado) => {
             expect(resultado.status).to.equal(200)
         })
     });
 
     it('Login com senha inválida', () => {
-        cy.request({
-            method: "POST",
-            url: "https://restful-booker.herokuapp.com/auth",
-            body: {
-                username: "admin",
-                password: "password23"
-            }
-        }).then((resultado) => {
+        cy.Login("admin","password23")
+        .then((resultado) => {
             expect(resultado.status).to.equal(200)
             expect(resultado.body.reason).to.equal("Bad credentials")
         })
     });
 
     it('Login com usuário inválido', () => {
-        cy.request({
-            method: "POST",
-            url: "https://restful-booker.herokuapp.com/auth",
-            body: {
-                username: "user",
-                password: "password123"
-            }
-        }).then((resultado) => {
+        cy.Login("user", "password123").then((resultado) => {
             expect(resultado.status).to.equal(200)
             expect(resultado.body.reason).to.equal("Bad credentials")
         })
